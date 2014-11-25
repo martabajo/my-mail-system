f38c0d774b0a34269fcb9068f62696e5af632ed3
martabajo <martabg2492@gmail.com>
2014-11-25 20:16:14 +0100

    crear un constructor que permita crear un objeto MailItem inicializando sus atributos por medio de parametros

--- MailItem.java
+++ MailItem.java
@@ -5,9 +5,24 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+/**
+ * 3 atributos de tipo `String` llamados `from`, `to` y `message`.
+ */
 public class MailItem
 {
    private String from;
    private String to;
    private String message;
+   
+   
+   /**
+    * Disponga de un constructor que permita crear un objeto `MailItem` inicializando 
+	sus atributos por medio de parámetros.
+    */
+   public MailItem(String newFrom, String newTo, String newMessage)
+    {
+        from = newFrom;
+        to = newTo;
+        message = newMessage;
+    }
 }
